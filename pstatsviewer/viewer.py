0c8711169d120272f0e01ff134482091c81487a3
Scott Sanderson <scoutoss@gmail.com>
2015-04-12 14:42:40 -0400

    DEV: Updates from PyCon talk.

--- pstatsviewer/viewer.py
+++ pstatsviewer/viewer.py
@@ -2,13 +2,13 @@
 A viewer for Stats objects.
 """
 import os
-from itertools import chain
 
-from IPython.display import display_html, display
-from IPython.html.widgets import interactive, IntSliderWidget
+from IPython.display import display
+from IPython.html.widgets import interactive, IntSlider
 import matplotlib.pyplot as plt
 import pandas as pd
 from pstats import Stats
+from qgrid import SlickGrid
 from six import iteritems
 import seaborn as sns
 sns.set_palette("deep", desat=.6)
@@ -61,7 +61,7 @@ def _calc_frames(stats):
     timings_df = pd.DataFrame(timings)
     callers_df = pd.DataFrame(callers)
     timings_df['filename:funcname'] = \
-        timings_df['filename'] + ':' + timings_df['funcname']
+        (timings_df['filename'] + ':' + timings_df['funcname'])
     timings_df = timings_df.groupby('filename:funcname').sum()
     return timings_df, callers_df
 
@@ -74,11 +74,12 @@ class StatsViewer(object):
         "cumtime",
     ]
 
-    def __init__(self, filename, strip_dirs=True):
+    def __init__(self, filename, strip_dirs=True, remote_js=False):
         self.name = os.path.basename(filename)
         stats = Stats(filename)
         self.stats = stats.strip_dirs() if strip_dirs else stats
         self.timings, self.callers = _calc_frames(stats)
+        self.remote_js = remote_js
 
     def summary(self, count):
         fig = plt.figure()
@@ -106,7 +107,7 @@ class StatsViewer(object):
 
     def _show_timing_data(self, data, sort, **mpl_kwargs):
         default_mpl_kwargs = {
-            'figsize': (14, 10),
+            'figsize': (12, 7),
             'kind': 'barh',
             'title': self._make_title(len(data), sort),
         }
@@ -120,39 +121,40 @@ class StatsViewer(object):
             plt.xlabel('Call Count')
         plt.ylabel('Filename:Function Name')
 
-    def view(self,
-             count,
-             sort,
-             fields=None,
-             show_table=True,
-             show_graph=True,
-             return_data=False,
-             **mpl_kwargs):
+    def _show_table(self, data):
+        display(SlickGrid(data, remote_js=self.remote_js))
+
+    def grid(self, fields=None):
+        if fields is None:
+            fields = self.default_view_fields
+
+        data = self.timings[fields]
+        self._show_table(data)
+
+    def chart(self, fields=None, **mpl_kwargs):
 
         if fields is None:
             fields = self.default_view_fields
-        data = self._get_timing_data(count, sort, fields)
 
-        if show_table:
-            # HACK: Make pandas always show an HTML Table repr for this frame.
-            data._repr_fits_horizontal_ = lambda *args, **kwargs: True
-            display_html(data._repr_html_(), raw=True)
-
-        if show_graph:
+        def _interact(count, sort_by):
+            data = self._get_timing_data(count, sort_by, fields)
             self._show_timing_data(
-                data.ix[::-1, sort],
-                sort,
+                data.ix[::-1, sort_by],
+                sort_by,
                 **mpl_kwargs
             )
 
-        if return_data:
-            return data
+        display(
+            interactive(
+                _interact,
+                count=IntSlider(min=5, max=100, step=5, value=20),
+                sort_by=('cumtime', 'tottime', 'ncalls'),
+            ),
+        )
 
-    def compare(self, other, field='cumtime', count=35):
+    def compare_chart(self, other, field='cumtime', count=35):
         left = self._get_timing_data(count, field, field)
-        left.name = "left"
         right = other._get_timing_data(count, field, field)
-        right.name = "right"
 
         fig = plt.figure()
         self._show_timing_data(
@@ -166,22 +168,28 @@ class StatsViewer(object):
             ax=fig.add_subplot('133')
         )
 
-        data = pd.concat([left, right], axis=1).fillna(0)
-        data['diff'] = data['left'] - data['right']
-        data = data.sort('diff', ascending=False)
+    def compare_grid(self, other, lsuffix='_l', rsuffix='_r'):
 
-        # HACK: Make pandas always show an HTML Table repr for this frame.
-        data._repr_fits_horizontal_ = lambda *args, **kwargs: True
-        display_html(data._repr_html_(), raw=True)
+        left = self.timings[self.default_view_fields]
+        right = other.timings[self.default_view_fields]
+
+        self._show_table(
+            left.join(
+                right,
+                lsuffix=lsuffix,
+                rsuffix=rsuffix,
+            ).sort_index(axis=1)
+        )
 
     def interact(self):
         def _interact(count, field):
-            self.view(count, field)
+            self.view(count, field, show_table=False)
 
         display(
+            SlickGrid(self.timings, self.remote_js),
             interactive(
                 _interact,
-                count=IntSliderWidget(min=5,max=100,step=5,value=35),
+                count=IntSlider(min=5, max=100, step=5, value=35),
                 field=('cumtime', 'tottime', 'ncalls'),
-            )
+            ),
         )
